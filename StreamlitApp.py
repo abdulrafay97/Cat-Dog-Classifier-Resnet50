1af5b28892f82afa4d4b1e05cd707afe1430a1a7
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2022-07-28 01:43:14 +0500

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -41,10 +41,16 @@ def Predict(img):
 #Get Image
 file_up = st.file_uploader('Upload an Image', type = "jpg")
 
+#Normalizing
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
 #Transforming the Image
 data_transform = transforms.Compose([
                 transforms.Resize((224, 224)),
-                transforms.ToTensor()])
+                transforms.ToTensor(),
+                normalize
+            ])
 
 if file_up is not None:
     # display image that user uploaded
